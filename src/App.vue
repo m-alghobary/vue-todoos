98c17d7fa404d0eade852a003077e3a0253b85dd
m-alghobary <m.alghobary90@gmail.com>
2020-12-11 21:42:47 +0300

    make task list component

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,22 @@
 		</div>
 		<div class="mt-12 flex gap-6 justify-between">
 			<span class="px-1 cursor-pointer text-gray-700 font-medium border-l-4 border-gray-300 pl-2 text-xs sm:text-sm">All (16)</span>
-			<span class="px-1 cursor-pointer text-gray-400 border-l-4 border-blue-400 pl-2 text-xs sm:text-sm">Open (11)</span>
-			<span class="px-1 cursor-pointer text-gray-400 border-l-4 border-yellow-300 pl-2 text-xs sm:text-sm">Inprogress (4)</span>
-			<span class="px-1 cursor-pointer text-gray-400 border-l-4 border-green-500 pl-2 text-xs sm:text-sm">Complete (1)</span>
+			<span class="px-1 cursor-pointer text-gray-400 font-light border-l-4 border-blue-400 pl-2 text-xs sm:text-sm">Open (11)</span>
+			<span class="px-1 cursor-pointer text-gray-400 font-light border-l-4 border-yellow-300 pl-2 text-xs sm:text-sm">Inprogress (4)</span>
+			<span class="px-1 cursor-pointer text-gray-400 font-light border-l-4 border-green-500 pl-2 text-xs sm:text-sm">Complete (1)</span>
 		</div>
 		<hr class="mt-4" />
-		<div class="space-y-3 mt-4">
-			<task-item v-for="task in tasks" :key="task.id" :task="task"></task-item>
+		<div class="mt-4">
+			<task-list :tasks="tasks"></task-list>
 		</div>
-		<app-dialog :isOpen="showEditDialog" @closed="showEditDialog = !showEditDialog"></app-dialog>
+		<!-- <app-dialog :isOpen="showEditDialog" @closed="showEditDialog = !showEditDialog"></app-dialog> -->
 	</div>
 </template>
 
 <script>
 import { reactive, ref } from 'vue';
 import NewTask from './components/new-task.vue';
-import TaskItem from './components/task-item.vue';
+import TaskList from './components/task-list.vue';
 import AppDialog from './components/app-dialog.vue';
 
 export default {
@@ -32,52 +32,19 @@ export default {
 	components: {
 		AppDialog,
 		NewTask,
-		TaskItem,
+		TaskList,
 	},
 	setup() {
 		const tasks = ref([]);
 		const showEditDialog = ref(false);
-		let currentTask = reactive({
-			id: 0,
-			title: '',
-			complete: false,
-			inProgress: false,
-		});
-
-		tasks.value.push({
-			id: 1,
-			title: 'Deploy website',
-			complete: false,
-			inProgress: false,
-		});
-
-		tasks.value.push({
-			id: 2,
-			title: 'Add new images',
-			complete: true,
-			inProgress: false,
-		});
-
-		tasks.value.push({
-			id: 2,
-			title: 'Redesign mobile app',
-			complete: false,
-			inProgress: true,
-		});
-
-		function editTask(task) {
-			showEditDialog.value = true;
-			currentTask = task;
-		}
 
 		function addTask(task) {
-			console.log(task);
+			tasks.value.push(task);
 		}
 
 		return {
 			tasks,
 			showEditDialog,
-			editTask,
 			addTask,
 		};
 	},
---
+++ src/components/task-list.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="space-y-3">
+		<template v-if="tasks.length">
+			<task-item v-for="task in tasks" :key="task.id" :task="task"></task-item>
+		</template>
+		<div v-else class="text-gray-600 text-center font-light">No tasks yet.</div>
+	</div>
+</template>
+
+<script>
+import TaskItem from './task-item.vue';
+
+export default {
+	components: {
+		TaskItem,
+	},
+	props: {
+		tasks: {
+			type: Array,
+			default: () => [],
+		},
+	},
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
